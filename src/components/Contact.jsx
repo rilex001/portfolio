5142fb165faa47431cc436f52973fe4c6cb385e2
rilex01 <=>
2020-07-04 00:27:18 +0200

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,8 @@ function Contact() {
         <div  id='contact'>
             <h3>Get In Touch</h3>
             <p class="projectinfo">Lorem ipsum dolor sit amet, consectetur.</p>
-            <form name='contact' netlify netlify-honeypot="bot-field" hidden>
+            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <input type="hidden" name="form-name" value="contact" />
                 <input type="text" name='name' placeholder='name' />
                 <input type="email" name='email' placeholder='EMAIL' />
                 <textarea name='message' placeholder='YOUR MESSAGE'></textarea>
