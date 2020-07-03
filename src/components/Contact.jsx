bf903fed73c199ee67b60e651c28a6d2e6d3154b
rilex01 <=>
2020-07-04 01:28:11 +0200

    contact finish

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,42 +1,16 @@
 import React from 'react'
-import NetlifyForm from 'react-netlify-form'
 
 function Contact() {
     return (
         <div  id='contact'>
-            <h3>Get In Touch</h3>
-            <NetlifyForm
-  name='Form With Recaptcha'
-  recaptcha={{
-    sitekey: 'my_recaptcha_site_key',
-    size: 'normal'
-  }}
->
-  {({ loading, error, recaptchaError, success, recaptcha }) => (
-    <div>
-      {loading &&
-        <div>Loading...</div>
-      }
-      {error &&
-        <div>Your information was not sent. Please try again later.</div>
-      }
-      {recaptchaError &&
-        <div>Recaptcha did not match. Please make sure the box is checked.</div>
-      }
-      {success &&
-        <div>Thank you for contacting us!</div>
-      }
-      {!loading && !success &&
-        <div>
-          <input type='text' name='Name' required />
-          <textarea name='Message' required />
-          {recaptcha}
-          <button>Submit</button>
-        </div>
-      }
-    </div>
-  )}
-</NetlifyForm>
+          <h3>Get In Touch</h3>
+        <form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="text" name="name" placeholder='NAME' />
+            <input type="email" name="email" placeholder='EMAIL' />
+            <textarea name="message" placeholder='YOUR MESSAGE' />
+            <button type="submit" className='button'>Send</button>
+        </form>
         </div>
     )
 }
