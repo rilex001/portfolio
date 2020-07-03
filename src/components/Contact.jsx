f6a7df90e5d2595a2ceb852923f41b1cb802e1a8
rilex001 <ristic1900@gmail.com>
2020-07-03 23:43:57 +0200

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
         <div  id='contact'>
             <h3>Get In Touch</h3>
             <p class="projectinfo">Lorem ipsum dolor sit amet, consectetur.</p>
-            <form name='contact' netlify>
+            <form name='contact' method="POST" data-netlify="true">
                 <input type="text" name='name' placeholder='name' />
                 <input type="email" name='email' placeholder='EMAIL' />
                 <textarea placeholder='YOUR MESSAGE'></textarea>
