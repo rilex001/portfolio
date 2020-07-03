3ba05a8f8d963c09c3a2ad8562ad4824e90ca178
rilex01 <=>
2020-07-04 00:30:07 +0200

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,12 +5,21 @@ function Contact() {
         <div  id='contact'>
             <h3>Get In Touch</h3>
             <p class="projectinfo">Lorem ipsum dolor sit amet, consectetur.</p>
-            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input type="hidden" name="form-name" value="contact" />
-                <input type="text" name='name' placeholder='name' />
-                <input type="email" name='email' placeholder='EMAIL' />
-                <textarea name='message' placeholder='YOUR MESSAGE'></textarea>
-                <div data-netlify-recaptcha='true'></div>
+            <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
                 <button class="button" type='submit'>Send</button>
             </form>
         </div>
