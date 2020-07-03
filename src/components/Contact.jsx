0e84b571d4328fd1422e3f985f8193bd15eff6ff
rilex01 <=>
2020-07-04 00:35:51 +0200

    proba

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,23 +5,10 @@ function Contact() {
         <div  id='contact'>
             <h3>Get In Touch</h3>
             <p class="projectinfo">Lorem ipsum dolor sit amet, consectetur.</p>
-            <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-                <button class="button" type='submit'>Send</button>
-            </form>
+            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+  {/* You still need to add the hidden input with the form name to your JSX form */}
+  <input type="hidden" name="form-name" value="contact" />
+  </form>
         </div>
     )
 }
