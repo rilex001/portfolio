104288b6b331fac7b192a0c4b2a89b60ba7ecb6e
rilex01 <=>
2020-07-04 02:57:17 +0200

    design tablet size

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,11 @@ function Project({name, info, picture, sitelink, codelink, stack}) {
     return (
         <>
             <h3 className='center'>{name}</h3>
+            <div className='project'>
+                <div>
             <img className='projectimage' src={picture} alt='video' />
+            </div>
+            <div>
             <p className='projectinfo'>{info}</p>
             <div className='stack'>
                 {stack && stack.map(item => <span className='program'><b>{item}</b></span>)}
@@ -13,6 +17,8 @@ function Project({name, info, picture, sitelink, codelink, stack}) {
             <div className='buttoncontainer center'>
             {sitelink.length > 1 ? <a className='button' href={sitelink} target='blank'>Visit site</a> : null}
             <a className='button' href={codelink} target='blank'>View code</a>
+            </div>
+            </div>
             </div>
         </>
     )
