d51ba82d1a3c63bb13864fc5ff72ed32f1cacfca
rilex01 <=>
2020-07-04 19:24:56 +0200

    style

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,10 +9,10 @@ function Info() {
         <div id="welcome" className="grid">
             <div className="welcome-text">
                 <p className="leading">My name is Nikola. I'm computer science student and web developer focused on crafting clean & user-friendly experiences</p>
-                <a className="button" href={pdf} target='_blank'>Download CV</a>
+                <a className="button" href={pdf} target='_blank' rel='noopener noreferrer'>Download CV</a>
                 <div className="mediacontainer">
-                    <div className='media'><a href="https://github.com/rilex001" target='_blank'><img src={ghicon} alt="facebook" className='iconimg'/></a></div>
-                    <div className='media'><a href="https://twitter.com/Nikola06213931" target='_blank'><img src={twicon} alt="twitter" className='iconimg'/></a></div>
+                    <div className='media'><a href="https://github.com/rilex001" target='_blank' rel='noopener noreferrer'><img src={ghicon} alt="facebook" className='iconimg'/></a></div>
+                    <div className='media'><a href="https://twitter.com/Nikola06213931" target='_blank' rel='noopener noreferrer'><img src={twicon} alt="twitter" className='iconimg'/></a></div>
                 </div>
             </div>
             <div className="welcome-img">
